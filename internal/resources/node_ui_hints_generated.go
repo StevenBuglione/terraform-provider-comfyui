01d74b2894d90ad0aca068aa6206dd8a2e4bf534
StevenBuglione <stevenbuglione1@gmail.com>
2026-04-11 23:40:26 -0400

    Refresh generated UI hints artifact

--- internal/resources/node_ui_hints_generated.go
+++ internal/resources/node_ui_hints_generated.go
@@ -20,7 +20,7 @@ type generatedNodeUIHint struct {
 }
 
 const GeneratedNodeUIHintsVersion = "1.0.0"
-const GeneratedNodeUIHintsExtractedAt = "2026-04-12T03:39:21.588Z"
+const GeneratedNodeUIHintsExtractedAt = "2026-04-12T03:40:34.876Z"
 const GeneratedNodeUIHintsComfyUICommit = "7782171a272bca4bfb8586a5bb801c5c04b5ee6a"
 const GeneratedNodeUIHintsComfyUIVersion = "unknown"
 const GeneratedNodeUIHintsCount = 662
